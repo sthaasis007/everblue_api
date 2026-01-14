66177debeddd89a92de3db836ffb64bfbea27b78
sthaasis007 <aashishshresth123@gmail.com>
2026-01-14 22:13:39 +0545

    updated

--- routes/customer.js
+++ routes/customer.js
@@ -7,8 +7,11 @@ const {
   updateStudent,
   deleteStudent,
   loginStudent,
-} = require("../controllers/student_controller");
+} = require("../controllers/customer_controller");
 
 router.post("/", createStudent);
+router.post("/login", loginStudent);
 router.put("/:id", protect, updateStudent);
 router.delete("/:id", protect, deleteStudent);
+
+module.exports = router;
